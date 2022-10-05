294f0212202f013bba7119568b5762f9c939d08c
EverVino <evervino00@gmail.com>
2022-10-05 00:16:00 -0400

    art juan carlos ttl control de tanque, modificación de la página about

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 
+
 module.exports = function(eleventyConfig) {
     // Haciendo una copia de los directorios a el dicrectorio publico ""public"
     eleventyConfig.addPassthroughCopy("./src/style.css");
@@ -12,6 +13,7 @@ module.exports = function(eleventyConfig) {
     // Adding to render math equations
     let markdownIt = require("markdown-it");
     let markdownItKatex = require("@iktakahiro/markdown-it-katex");
+    let markdownItAttrs = require("markdown-it-attrs");
     let options = {
         html: true,
         breaks: true,
@@ -20,6 +22,7 @@ module.exports = function(eleventyConfig) {
     };
     let markdownLib = markdownIt(options)
     markdownLib.use(markdownItKatex);
+    markdownLib.use(markdownItAttrs);
     
     eleventyConfig.setLibrary("md", markdownLib);
     //Date Clean up
